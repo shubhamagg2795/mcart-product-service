a81d2b67ebcd84e68ac351f27835265dad0c4558
Shubham Aggarwal <shubham.agg2795@gmail.com>
2026-02-24 22:45:21 +0530

    Initial Spring Boot Product Service setup

---
+++ src/main/java/com/mcart/productservice/McartProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mcart.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McartProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McartProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcart/productservice/McartProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcart.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McartProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
