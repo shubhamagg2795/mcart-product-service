5c6a23edf6913d6fe0f93cfad97094f5132d7bf4
Shubham Aggarwal <shubham.agg2795@gmail.com>
2026-03-05 16:01:12 +0530

    Changes to add /api in products controller

--- src/main/java/com/mcart/productservice/controller/ProductController.java
+++ src/main/java/com/mcart/productservice/controller/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/products")
 @CrossOrigin(origins = "*")  // temporary
 public class ProductController {
 
