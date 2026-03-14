14cf530f12e72d9f67dae1aa046c2fbef5fb32d5
Shubham Aggarwal <shubham.agg2795@gmail.com>
2026-03-14 22:03:55 +0530

    updated fixed version

---
+++ src/main/java/com/mcart/productservice/controller/VersionController.java
@@ -0,0 +1,15 @@
+package com.mcart.productservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class VersionController {
+
+    @GetMapping("/version")
+    public String version() {
+        return "Product Service CI/CD Version 1";
+    }
+}
